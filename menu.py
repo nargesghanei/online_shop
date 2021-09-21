4ce812b7aaeb7fa726e2534a3eb2916ec0975bdd
nargesghanei <nargesghanei031@gmail.com>
2021-09-21 15:09:44 +0430

    add manager registration

--- menu.py
+++ menu.py
@@ -1,54 +1,79 @@
-import tkinter as tk
+# import tkinter as tk
+#
+#
+# def btn_click(choice):
+#     if choice == 'register':
+#         Register()
+#     elif choice == 'enter':
+#         Enter()
+#     elif choice == 'exit':
+#         Exit()
+#
+#
+# class Register:
+#     def __init__(self):
+#         print('registered')
+#         window = tk.Tk()
+#         frame1 = tk.Frame(master=window, width=400, height=100, bg='blue')
+#         frame1.pack()
+#         label1 = tk.Label(master=frame1, text="Enter your info", font=('arial', 30), bg="white")
+#         label1.place(x=0, y=0)
+#
+#
+# class Enter:
+#     def __init__(self):
+#         print('entered')
+#         window = tk.Tk()
+#         frame2 = tk.Frame(master=window, width=400, height=100, bg="green")
+#         frame2.pack()
+#         label2 = tk.Label(master=frame2, text="Enter your info", font=('arial', 30), bg="white")
+#         label2.place(x=0, y=0)
+#
+#
+# class Exit:
+#     def __init__(self):
+#         print('exited')
+#         window = tk.Tk()
+#         frame3 = tk.Frame(master=window, width=400, height=100, bg="red")
+#         frame3.pack()
+#         label3 = tk.Label(master=frame3, text="Exit", font=('arial', 30), bg="white")
+#         label3.place(x=0, y=0)
+#
+#
+# cal = tk.Tk()
+# cal.title("MENU")
+#
+# btn_register = tk.Button(cal, padx=160, pady=20, bd=10, fg='blue', font=('arial', 20, 'bold'),
+#                          text="Do not have an account?Register", command=lambda: btn_click('register')).grid(row=0,
+#                                                                                                              column=0)
+# btn_enter = tk.Button(cal, padx=160, pady=20, bd=10, fg='green', font=('arial', 20, 'bold'),
+#                       text='Already have an account? Enter', command=lambda: btn_click('enter')).grid(row=1, column=0)
+# btn_exit = tk.Button(cal, padx=160, pady=20, bd=10, fg='red', font=('arial', 20, 'bold'),
+#                      text='Exit', command=lambda: btn_click('exit')).grid(row=2, column=0)
+#
+# cal.mainloop()
+import register
 
 
-def btn_click(choice):
-    if choice == 'register':
-        Register()
-    elif choice == 'enter':
-        Enter()
-    elif choice == 'exit':
-        Exit()
+def menu():
+    choice = 0
+    while choice != 3:
+        print("1-Don't you have an account? Register.")
+        print("2-Already have an account? Enter.")
+        print("3-Exit.")
+        print()
+        try:
+            choice = int(input("Please Enter your option:  "))
+            if not 1 <= choice <= 3:
+                raise Exception("There is not such an option!")
+        except Exception as error:
+            print(f"{error}, Please Enter number between 1 to 3!")
+        if choice == 1:
+            register.Register()
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            print("Have a great time. Goodbye!")
 
 
-class Register:
-    def __init__(self):
-        print('registered')
-        window = tk.Tk()
-        frame1 = tk.Frame(master=window, width=400, height=100, bg='blue')
-        frame1.pack()
-        label1 = tk.Label(master=frame1, text="Enter your info", font=('arial', 30), bg="white")
-        label1.place(x=0, y=0)
-
-
-class Enter:
-    def __init__(self):
-        print('entered')
-        window = tk.Tk()
-        frame2 = tk.Frame(master=window, width=400, height=100, bg="green")
-        frame2.pack()
-        label2 = tk.Label(master=frame2, text="Enter your info", font=('arial', 30), bg="white")
-        label2.place(x=0, y=0)
-
-
-class Exit:
-    def __init__(self):
-        print('exited')
-        window = tk.Tk()
-        frame3 = tk.Frame(master=window, width=400, height=100, bg="red")
-        frame3.pack()
-        label3 = tk.Label(master=frame3, text="Exit", font=('arial', 30), bg="white")
-        label3.place(x=0, y=0)
-
-
-cal = tk.Tk()
-cal.title("MENU")
-
-btn_register = tk.Button(cal, padx=160, pady=20, bd=10, fg='blue', font=('arial', 20, 'bold'),
-                         text="Do not have an account?Register", command=lambda: btn_click('register')).grid(row=0,
-                                                                                                             column=0)
-btn_enter = tk.Button(cal, padx=160, pady=20, bd=10, fg='green', font=('arial', 20, 'bold'),
-                      text='Already have an account? Enter', command=lambda: btn_click('enter')).grid(row=1, column=0)
-btn_exit = tk.Button(cal, padx=160, pady=20, bd=10, fg='red', font=('arial', 20, 'bold'),
-                     text='Exit', command=lambda: btn_click('exit')).grid(row=2, column=0)
-
-cal.mainloop()
+menu()
---
+++ register.py
@@ -0,0 +1,149 @@
+class Register:
+    def __init__(self):
+        choice = 0
+        self.pass_word = None
+        self.pass_word_again = False
+        self.user_name = None
+        self.shop_name = None
+        self.work_hour_m = None
+        self.work_hour = None
+        while choice != 3:
+            print("1-Register as a manager.")
+            print("2-Register as a client.")
+            print("3-Back.")
+            print()
+            try:
+                choice = int(input("Please Enter your option:  "))
+                if not 1 <= choice <= 3:
+                    raise Exception("There is not such an option!")
+            except Exception as error:
+                print(f"{error}, Please Enter number between 1 to 3!")
+            if choice == 1:
+                self.managers_registration()
+            elif choice == 2:
+                pass
+
+    def managers_registration(self):
+        all_true = True
+        pass_again = False
+        name_of_shop = False
+        while all_true:
+            if not self.user_name:
+                user_name = input("Please Enter your phone number as your user name: ")
+                valid_user_name = Register.user_name_validation(user_name)
+                if not valid_user_name:
+                    continue
+                else:
+                    self.user_name = user_name
+            if not self.pass_word:
+                pass_word = input("Please Enter a password: ")
+                valid_pass_word = self.pass_word_validation(pass_word)
+                if not valid_pass_word:
+                    continue
+                else:
+                    self.pass_word = pass_word
+            if not self.pass_word_again:
+                password_again = input("Please Enter your password again: ")
+                try:
+                    if self.pass_word == password_again:
+                        pass_again = True
+                        print("Password saved successfully!")
+                    else:
+                        pass_again = False
+                        raise Exception("Please enter the same password!")
+                except Exception as error:
+                    print(f"{error} Please try again!")
+                if not pass_again:
+                    continue
+                else:
+                    self.pass_word_again = True
+            if not self.shop_name:
+                shop_name = input("Please Enter the name of your shop center: ")
+                try:
+                    if shop_name:
+                        name_of_shop = True
+                        print("Shop name accepted!")
+                    else:
+                        name_of_shop = False
+                        raise Exception("You should at least enter sth!")
+                except Exception as error:
+                    print(f"{error} Please try again.")
+                if not name_of_shop:
+                    continue
+                else:
+                    self.shop_name = shop_name
+            if not self.work_hour_m:
+                work_hour = tuple(input("Enter the opening and closing time, (ex: 08:30,18:45): ").split(','))
+                work_hour_last = Register.time_validate(work_hour[0], work_hour[1])
+                if work_hour_last:
+                    print("Work interval accepted!")
+                    self.work_hour_m = work_hour_last
+                    self.work_hour = work_hour
+                    all_true = False
+                else:
+                    continue
+        print(f"\nManager registered successfully with \nusername: {self.user_name}\
+            \nshop name: {self.shop_name}\nwork_hour: {self.work_hour[0]} to {self.work_hour[1]}\n")
+
+    @staticmethod
+    def user_name_validation(user_name):
+        try:
+            if user_name.isnumeric() and len(user_name) == 11:
+                print("User name accepted!")
+                return True
+            else:
+                raise Exception("Invalid username!")
+        except Exception as error:
+            print(f"{error}, Please try again!")
+        return False
+
+    @staticmethod
+    def pass_word_validation(pass_word):
+        val = True
+        try:
+            special_sym = ['$', '@', '#', '%', '_']
+
+            if len(pass_word) < 8:
+                val = False
+                raise Exception('Password should be at least 8 characters!')
+
+            elif len(pass_word) > 20:
+                val = False
+                raise Exception('Password should not be greater than 20 characters!')
+
+            elif not any(char.isdigit() for char in pass_word):
+                val = False
+                raise Exception('Password should have at least one numeral!')
+
+            elif not any(char.isupper() for char in pass_word):
+                val = False
+                raise Exception('Password should have at least one uppercase letter')
+
+            elif not any(char.islower() for char in pass_word):
+                val = False
+                raise Exception('Password should have at least one lowercase letter')
+
+            elif not any(char in special_sym for char in pass_word):
+                val = False
+                raise Exception('Password should have at least one of the symbols $@#%_')
+
+        except Exception as error:
+            print(f"{error}, Please try again!")
+
+        if val:
+            print("Password accepted!")
+        return val
+
+    @staticmethod
+    def time_validate(opening, closing):
+        try:
+            time1 = list(map(int, opening.split(":")))
+            opening = time1[0] * 60 + time1[1]
+            time2 = list(map(int, closing.split(":")))
+            closing = time2[0] * 60 + time2[1]
+            if 0 > time1[0] > 24 or 0 > time2[0] > 24 or 0 > time1[1] > 59 or 0 > time2[1] > 59:
+                raise Exception("Invalid time")
+        except Exception as error:
+            print(f"{error}. Please try again!")
+            return False
+        return opening, closing
